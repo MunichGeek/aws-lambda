782452e842b55f86ab8820ae6fa687ff61ee2157
MunichGeek <munichfarid@gmail.com>
2022-11-12 13:30:22 +0100

    A minor change.

--- test-function.py
+++ test-function.py
@@ -3,4 +3,4 @@ import json
 
 def lambda_handler(event, context):
     print("Hey! I just added this Lambda with Terraform")
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+    return {"statusCode": 200, "body": json.dumps("Hello from Munich Geek!")}
